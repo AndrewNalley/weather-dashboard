3de74a9f5f80bcd2bd84f3fe441283b17d14131d
Andrew Nalley <awnalley@gmail.com>
2023-05-17 16:59:10 -0500

    organize weather data

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,13 @@ var apiNinjaKey = 'c6zYH9j97tY8e4IyXXNHfA==TrC4jZ68YEo2FrrD'
 var search = document.getElementById('search')
 var current = document.getElementById('current')
 var future = document.getElementById('future')
+var currentDay = document.getElementById('current-day')
+var day1 = document.getElementById('day-1')
+var day2 = document.getElementById('day-2')
+var day3 = document.getElementById('day-3')
+var day4 = document.getElementById('day-4')
+var day5 = document.getElementById('day-5')
+
 
 function getWeather(lat, lon) {
     var weatherAPI = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${weatherKey}`
@@ -15,9 +22,26 @@ function getWeather(lat, lon) {
             }
             return response.json()
         })
-        .then(function (data) {
+        .then(data => {
+            if (data) {
+                console.log(data)
+            var data0 = data.list[0]
+            var cityName = data.city.name
+            var weatherDate = dayjs.unix(data0.dt)
+            var weatherIcon = data0.weather.icon 
+            var temp = data0.main.temp 
+            var tempFeelsLike = data0.main.feels_like
+            var humidity = data0.main.humidity 
+            var windSpeed = data0.wind.speed 
+            var windDirection = data0.wind.deg
+            console.log(cityName, weatherDate, weatherIcon, temp, tempFeelsLike, humidity, windSpeed, windDirection)
+            } else {
+                console.log("No city found")
+            }
             // display data in present and future sections on html page
-            console.log(data)
+            // needs city name, date, weather condition icon, temp, humidity, wind speed
+            // Current, 5-day future
+            //search history (logged in local)
         })
         .catch((error) => console.log('Error: ', error));
 }
@@ -50,7 +74,7 @@ function userSearch(city, state, country) {
             var location = result[0]
             var lat = location.latitude
             var lon = location.longitude
-            console.log(result);
+            console.log(location);
             } else {
                 console.log("No location found in response.")
             }
